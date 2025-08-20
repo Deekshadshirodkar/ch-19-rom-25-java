fb300fbbe085dfe5c4d4d6dc6ec510370455e0f0
Deeksha Shirodkar <deekshashirodkar40@gmail.com>
2025-08-20 23:12:42 +0530

    initial Commit

---
+++ SwapNumbers.java
@@ -0,0 +1,16 @@
+class SwapNumbers{
+	
+	public static void main(String[] args){
+		
+		int num1 = 10, num2 = 20;
+		
+		System.out.println("Before Swapping : num1 = " + num1 + ", num2 = " + num2);
+		
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+		
+		System.out.println("After Swapping: num1 = " +num1 + ", num2 = " +num2 );
+		
+	}
+}
